7286d389198077ae10f013767a11a115d05332a9
Larissa Falcão <larissafalcao@outlook.com>
2025-03-28 11:50:50 -0300

    feat: project basic structure

---
+++ src/main/java/br/com/larissafalcao/process_images_api/ProcessImagesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.larissafalcao.process_images_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessImagesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessImagesApiApplication.class, args);
+	}
+
+}
